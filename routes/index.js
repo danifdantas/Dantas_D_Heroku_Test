a205927b4e50390d00af33c1c5ad23414c94089c
Dani Dantas <dani@DaniVonRohr.local>
2019-03-18 17:32:53 -0400

    initial styles for mobile

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
+const data = require(`../data`);
+
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express', message: "Welcome to your first Express app" });
+  console.log('data:', data);
+
+  res.render('index', data);
 });
 
 module.exports = router;
